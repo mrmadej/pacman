b2524a29cab2cabfb0a1a776f907251ccccaf415
Marcin Madej <marcin.r.madej@gmail.com>
2023-05-19 12:34:37 +0200

    Rysowanie planszy do gry po wcisnieciu start

--- pacman.py
+++ pacman.py
@@ -1,12 +1,14 @@
 import pygame, os
+from board import *
+import math
 
 pygame.init()
 
 window_open = True
 # wymiary okna
-WIDTH = 1280
-HEIGHT = 720
-
+WIDTH = 900
+HEIGHT = 950
+PI = math.pi
 
 # Ustalenie ścieżki do obrazka
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,13 +28,49 @@ image_path = os.path.join(current_dir, 'Start_button.png')
 button_image = pygame.image.load(image_path).convert()
 
 # Ustalenie pozycji przycisku
-button_x = 471.5
+button_x = 281.5
 button_y = 500
 button_width = 337
 button_height = 130
 
 # Utworzenie przycisku
 button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
+
+level = boards
+
+color = 'blue'
+
+def draw_board():
+    num1 = ((HEIGHT - 50) // 32)
+    num2 = (WIDTH // 30)
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 4)
+            if level[i][j] == 2:
+                pygame.draw.circle(screen, 'white', (j * num2 + (0.5 * num2), i * num1 + (0.5 * num1)), 10)
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j * num2 + (0.5 * num2), i * num1),
+                                 (j * num2 + (0.5 * num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j * num2 - (num2 * 0.4)) - 2, (i * num1 + (0.5 * num1)), num2, num1],
+                                0, PI / 2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
+            if level[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                 (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 
 screen.fill((0, 0, 0))
@@ -54,20 +92,21 @@ while window_open:
 
     
     
-    screen.fill((255, 255, 255))  # Czyszczenie ekranu
+    screen.fill((0, 0, 0))  # Czyszczenie ekranu
     
     # Rysowanie na ekranie
-    screen.blit(background_image, (0, 0))  # Wyświetlanie obrazka tła
+    screen.blit(background_image, (90.5, 0))  # Wyświetlanie obrazka tła
     screen.blit(button_image, button_rect)  # Wyświetlanie obrazka na przycisku
 
 
     if is_game_running:
         # Renderowanie planszy gry
 
-        # Ustawienie koloru tła na czerwony
-        screen.fill((255, 0, 0))
-    
-        # Renderowanie ekranu startowego
+        # Ustawienie koloru tła na czarny
+        screen.fill((0, 0, 0))
+        # Rysowanie planszy
+        draw_board()
+        
 
     # Aktualizacja ekranu
     pygame.display.flip()
