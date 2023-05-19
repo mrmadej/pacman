ee5ba6f7bf6c7ad142b61441786caa29646ade57
Marcin Madej <marcin.r.madej@gmail.com>
2023-05-19 11:09:06 +0200

    Dodanie planszy gry po wcisnieciu start

--- pacman.py
+++ pacman.py
@@ -3,6 +3,7 @@ import pygame, os
 pygame.init()
 
 window_open = True
+# wymiary okna
 WIDTH = 1280
 HEIGHT = 720
 
@@ -14,26 +15,28 @@ image_path = os.path.join(current_dir, 'Menu_background.jpg')
 
 
 
-# otwieranie okienka o rozdzielczości 640x840
+# otwieranie okienka
 screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 # To się wyświetla na górze jako nazwa programu
 pygame.display.set_caption("Pacman")
-# wypełnia ekran na podany kolor rgb (w tym wypadku czarny)
+
+# podkłada grafikę do tła i przycisku
 background_image = pygame.image.load(image_path).convert()
 image_path = os.path.join(current_dir, 'Start_button.png')
 button_image = pygame.image.load(image_path).convert()
 
 # Ustalenie pozycji przycisku
-button_x = 440
-button_y = 400
-button_width = 200
-button_height = 50
+button_x = 471.5
+button_y = 500
+button_width = 337
+button_height = 130
 
 # Utworzenie przycisku
 button_rect = pygame.Rect(button_x, button_y, button_width, button_height)
 
 
 screen.fill((0, 0, 0))
+is_game_running = False
 
 while window_open:
     # wyłączanie gry
@@ -43,12 +46,28 @@ while window_open:
                 window_open = False
         elif event.type == pygame.QUIT:
             window_open = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if button_rect.collidepoint(mouse_pos):
+                is_game_running = True
+
+    
     
     screen.fill((255, 255, 255))  # Czyszczenie ekranu
-     # Rysowanie na ekranie
+    
+    # Rysowanie na ekranie
     screen.blit(background_image, (0, 0))  # Wyświetlanie obrazka tła
     screen.blit(button_image, button_rect)  # Wyświetlanie obrazka na przycisku
 
+
+    if is_game_running:
+        # Renderowanie planszy gry
+
+        # Ustawienie koloru tła na czerwony
+        screen.fill((255, 0, 0))
+    
+        # Renderowanie ekranu startowego
 
     # Aktualizacja ekranu
     pygame.display.flip()
